3dac358f11a7135088c283ea1fe1603231df33c1
Zafnshr <abdalrahmanhani30@gmail.com>
2026-07-17 00:57:02 +0300

    feat: create login page component for user authentication

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -16,7 +16,7 @@ export default function LoginPage() {
           const res = await signIn("credentials", {
             email,
             password,
-            callbackUrl: "/curriculum",
+            callbackUrl: "/dashboard",
             redirect: false,
           });
 
